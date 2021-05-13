bcc8f587aef6b51ce6825b2963a5b1e5dc4a88cd
randychoi95 <jhc130833@naver.com>
2021-05-13 16:26:59 +0900

    add memory storage

---
+++ RxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,23 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by 최제환 on 2021/05/13.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
